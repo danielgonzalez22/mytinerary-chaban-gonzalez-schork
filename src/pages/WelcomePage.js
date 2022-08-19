fc631133fc3bfc68b58094366e90201bc4dba0be
danielgonzalez22 <kavellid63@gmail.com>
2022-08-19 07:10:38 -0300

    correccion de nombres de clases de welcomepage js

--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -2,9 +2,9 @@ import '../styles/WelcomePage.css';
 
 function WelcomePage() {
   return (
-    <div className="App-main-container">
-      <h1 className="App-title">MyTinerary</h1>
-      <button className="App-button">start journey</button>
+    <div className="WelcomePage-main-container">
+      <h1 className="WelcomePage-title">MyTinerary</h1>
+      <button className="WelcomePage-button">start journey</button>
     </div>
   );
 }
