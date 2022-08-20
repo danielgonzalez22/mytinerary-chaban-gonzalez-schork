3be8dcaba0c0f16db18e2990e3be1d64902b86f0
lulielizalde <lulielizalde6@gmail.com>
2022-08-20 18:14:29 -0300

    style listo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,15 +3,18 @@ import "../styles/Footer.css"
 function Footer(){
     return(
 <div className="footer-container">
-    <h3>MindHub 2022 </h3>
+    <h4>MindHub 2022 </h4>
     <div className="redes-sociales-footer">
-        <img src="/img/facebook.svg" alt="facebookIcon"/>
-        <img src="/img/whatsapp.svg" alt="whatsappIcon"/>
-        <img src="/img/instagram.svg" alt="instagramIcon"/>
+        <img src="/img/facebook.svg" alt="facebookIcon" className="socials"/>
+        <img src="/img/whatsapp.svg" alt="whatsappIcon" className="socials"/>
+        <img src="/img/instagram.svg" alt="instagramIcon" className="socials"/>
     </div>
     <div className="metodos-pago-footer">
-        <h3>Metodos de Pago</h3>
+        <h4>Payment Methods:</h4>
         <img src="/img/metodosdepago.png" alt="instagramIcon" className="metodos-style-footer"/>
+    </div>
+    <div className="copyright-container">
+        <p>Copyright © - MYTINERARY All Rights Reserved.</p>
     </div>
 </div>
     )
