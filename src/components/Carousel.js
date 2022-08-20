aca6e8a23dec7a079249461b490cb7e5415ab8d8
lulielizalde <lulielizalde6@gmail.com>
2022-08-20 19:35:29 -0300

    estilo final

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -7,13 +7,14 @@ const items = props.data
   
   const itemView = (item) => (
     <div className="item">
-      <img src={item.url} alt={item.title} />
-      <p>{item.title}</p>
+      <img src={item.url} alt={item.title} className = "image-style"/>
+      <p className="title-city">{item.title}</p>
     </div>
   )
   return (
     <>
       <div className="slide">
+        <h2 className="popular-subtitle">{props.title}</h2>
         {items.slice(start, end).map(itemView)}
       </div>
     </>
---
+++ src/components/UnderConstruction.js
@@ -0,0 +1,9 @@
+import '../styles/UnderConstruction.css';
+function UnderConstruction() {
+  return (
+      <div>
+        <p className='under-cons'>Under Construction!</p>
+      </div>
+  );
+}
+export default UnderConstruction;
