7e66468231de4e6c8589c1f8bbd057bcaf5c0c9d
danielgonzalez22 <kavellid63@gmail.com>
2022-08-20 15:21:46 -0300

    correcion de paths de imagenes del header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,14 @@ function Header(){
     return(
 <div className="header-container">
     <div className="icon">
-        <img src="" alt="headerIcon" />
+        <img src="\img\suitcase.svg" alt="headerIcon" />
     </div>
     <nav>
         <a className="navLink" href="../../public/index.html">Home</a>
         <a className="navLink" href="../../public/index.html">Cities</a>
     </nav>
     <div className="accounts">
-        <img src="../img/user.svg" alt="accountIcon" />
+        <img src="/img/user.svg" alt="accountIcon" />
     </div>
 </div>
     )
