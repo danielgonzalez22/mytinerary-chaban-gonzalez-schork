cd3537ea136b2b1e1b1aab9352459ca574b2e108
danielgonzalez22 <kavellid63@gmail.com>
2022-08-24 00:28:10 -0300

    scrolltotop added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import WelcomePage from "./pages/WelcomePage";
 import Cities from "./pages/Cities";
 import NewCity from "./pages/NewCity";
-
+import ScrollToTop from "./components/ScrollToTop";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import React from "react";
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
+    <ScrollToTop/>      
+    <Routes>
         <Route path='/' element={<WelcomePage/>}></Route>
         <Route path='/Cities' element={<Cities/>}></Route>
         <Route path='/NewCity' element={<NewCity/>}></Route>
