00930dffbd67ecea3c8b7c4b4def9ac15559200e
danielgonzalez22 <kavellid63@gmail.com>
2022-08-24 12:35:15 -0300

    scroll to top utomatico y manual

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,13 +1,46 @@
-import React, {useEffect} from 'react'
-import {useLocation} from 'react-router-dom'
+import React, {useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+//import "../styles/ScrollToTop.css"
 
-export default function ScrollToTop() {
-    const {pathname} = useLocation()
-    console.log(pathname)
-    useEffect(() =>{
-        window.scrollTo(0, 0)
-    }, [pathname])
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  //al utilizar algun nav, carga cada pagina directamente en top
+  useEffect(() => {
+    window.scrollTo({
+      top:0,
+      behavior: "smooth"
+    })
+  }, [pathname])
+
+  //metodo para scrollear a top de manera suave
+  function goToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }
+
   return (
-    <button>Scroll to Top</button>
+        <button className='scroll-to-top' onClick={goToTop}>Scroll to Top</button>
   )
 }
+//import React, {useState, useEffect} from 'react'
+//const [showTopBtn, setShowTopBtn] = useState(false)
+// useEffect(() => {
+//   window.addEventListener("scroll", () => {
+//     if (window.scrollY > 400) {
+//       setShowTopBtn(true);
+//     } else {
+//       setShowTopBtn(false);
+//     }
+//   });
+// }, [])
+// return (
+//   <div>
+//     {" "}
+//     {showTopBtn && (
+//       <button onClick={goToTop}>Scroll to Top</button>
+//     )} {" "}
+//   </div>
+// )
+export default ScrollToTop
