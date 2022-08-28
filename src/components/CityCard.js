02b9c43efcc7ccc8415194634493b48c76e3f8e8
danielgonzalez22 <kavellid63@gmail.com>
2022-08-28 13:55:44 -0300

    cards generadas dinamicamente en pagina Cities

--- src/components/CityCard.js
+++ src/components/CityCard.js
@@ -1,1 +1,12 @@
 import "../styles/CityCard.css"
+
+export default function CityCard(props) {
+
+  return (
+    <div className="card">
+      <img src={props.image} alt="" className="card-img"/>
+      <p className="card-title">{props.title}</p>
+      <button className="card-button" type="">See more...</button>
+    </div>
+  )
+}
