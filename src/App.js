521818825e3532c411c2460959a080a90e65ee21
danielgonzalez22 <kavellid63@gmail.com>
2022-09-01 03:46:21 -0300

    route de City agregada

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import WelcomePage from "./pages/WelcomePage"
 import Cities from "./pages/Cities"
 import NewCity from "./pages/NewCity"
+import City from "./pages/City"
 import NotFound from "./pages/NotFound"
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import React from "react";
@@ -11,6 +12,7 @@ function App() {
         <Route path='/' element={<WelcomePage/>}></Route>
         <Route path='/Cities' element={<Cities/>}></Route>
         <Route path='/NewCity' element={<NewCity/>}></Route>
+        <Route path='/City' element={<City/>}></Route>
         <Route path='/*' element={<NotFound/>}></Route>
       </Routes>
     </BrowserRouter>
