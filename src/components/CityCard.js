0d7022dec2667ab332e2e7709c46ca7836334202
danielgonzalez22 <kavellid63@gmail.com>
2022-09-01 03:16:58 -0300

    añadido direccionar a pagina not found al clickear en card de pagina cities

--- src/components/CityCard.js
+++ src/components/CityCard.js
@@ -1,4 +1,5 @@
 import "../styles/CityCard.css"
+import { Link as LinkRouter } from 'react-router-dom'
 
 export default function CityCard(props) {
 
@@ -6,7 +7,7 @@ export default function CityCard(props) {
     <div className="card">
       <img src={props.image} alt="" className="card-img"/>
       <p className="card-title">{props.title}</p>
-      <button className="card-button" type="">See more...</button>
+      <LinkRouter to='/*'><button className="card-button" type="">See more...</button></LinkRouter>
     </div>
   )
 }
