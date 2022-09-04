a865f2700e93b5d2aa7979b10ea85f637f138211
lulielizalde <lulielizalde6@gmail.com>
2022-09-04 17:57:10 -0300

    edit city

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Cities from "./pages/Cities"
 import NewCity from "./pages/NewCity"
 import City from "./pages/City"
 import NotFound from "./pages/NotFound"
+import EditCity from "./pages/EditCity"
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import React from "react";
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path='/Cities' element={<Cities/>}></Route>
         <Route path='/NewCity' element={<NewCity/>}></Route>
         <Route path='/City' element={<City/>}></Route>
+        <Route path='/EditCity' element={<EditCity/>}></Route>
         <Route path='/*' element={<NotFound/>}></Route>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/EditCity.js
@@ -0,0 +1,48 @@
+import '../styles/EditCity.css';
+import WebsiteLayout from '../layouts/WebsiteLayout';
+import Input from '../components/Input';
+import * as React from 'react';
+function EditCity() {
+  const model = ["Image URL","City Name","Country", "Description","Population","Foundation Year"];
+
+  const imageRef = React.useRef();
+  const cityRef = React.useRef();
+  const countryRef = React.useRef();
+  const descriptionRef = React.useRef();
+  const populationRef = React.useRef();
+  const foundationRef = React.useRef();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(imageRef.current.value);
+    console.log(cityRef.current.value);
+    console.log(countryRef.current.value);
+    console.log(descriptionRef.current.value);
+    console.log(populationRef.current.value);
+    console.log(foundationRef.current.value);
+  }
+
+  return (
+    <WebsiteLayout>
+      <div className='newcity-body'>
+        <div className='title-form-page'>
+          <h1 className='title-form'>Edit A<span className='my-style'> City</span>!</h1>
+          <img src="/img/gummy-city.svg" alt="icon" className='city-form' />
+        </div>
+        <form className='form' onSubmit={handleSubmit}>
+        <select className='input'>
+            <option></option>
+        </select>
+        <Input text="Image Url" reference = {imageRef}></Input>
+        <Input text="City Name" reference = {cityRef}></Input>
+        <Input text="Country" reference = {countryRef}></Input>
+        <Input text="Description" reference = {descriptionRef}></Input>
+        <Input text="Population" reference = {populationRef}></Input>
+        <Input text="Foundation Year" reference = {foundationRef}></Input>
+        <button className="welcomePage-button">Send</button>
+        </form>
+      </div>
+    </WebsiteLayout>
+  );
+}
+export default EditCity;
