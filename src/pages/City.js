b866488e08b3defa9b576e5b755fba42b65ea2d0
danielgonzalez22 <kavellid63@gmail.com>
2022-09-07 02:16:10 -0300

    descripcion añadida a City (details)

--- src/pages/City.js
+++ src/pages/City.js
@@ -1,10 +1,8 @@
 import WebsiteLayout from '../layouts/WebsiteLayout'
 import '../styles/City.css'
-import { Link as LinkRouter } from 'react-router-dom'
 import React, { useEffect, useState } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import axios from 'axios'
-
 
 export default function City() {
   const [city, setCity] = useState({})
@@ -15,24 +13,6 @@ export default function City() {
       .then(res => setCity(res.data.response))
       // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
-  // const getCity = async () => {
-  //     try {
-  //         const res = await axios.get('http://localhost:4000/cities/' + {id})
-
-  //     if (res.data){
-  //         return setCity(res.data)
-  //     }
-  //     // throw new Error ("Ups! An error ocurred")
-  //     }catch (error){
-  //         return console.log(error.message);
-  //     }
-  // }
-
-  console.log(city);
-
-  // useEffect(() => {
-  //     getCity()
-  // }, [])
   const navigate = useNavigate();
   return (
     <WebsiteLayout>
@@ -46,8 +26,8 @@ export default function City() {
               <li>Population:{city.population}</li>
               <li>Foundation:{city.foundation}</li>
             </ul>
-            {/* <h3>Description</h3>
-            <p className='description-p'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae, nihil, consectetur molestias autem est earum porro nulla neque sapiente tempora, atque quod quae natus? Voluptates nesciunt sunt id sequi officiis, consequatur impedit, perspiciatis sit, vel ex est quae corrupti mollitia ut necessitatibus a explicabo deleniti repudiandae tempora quo aliquid dolor!</p> */}
+            <h3>Description</h3>
+            <p className='description-p'>{city.description}</p>
           </div>
           <button onClick={() => navigate(-1)}>Go back</button>
         </div>
