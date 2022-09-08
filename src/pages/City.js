98dd1cf95dac757aee4011599b69e65e83636c52
danielgonzalez22 <kavellid63@gmail.com>
2022-09-08 15:41:43 -0300

    estilos itinerary, intento de mostrar usuario de itinerario

--- src/pages/City.js
+++ src/pages/City.js
@@ -13,14 +13,14 @@ export default function City() {
   useEffect(() => {
     axios.get("http://localhost:4000/cities/" + [id])
       .then(res => setCity(res.data.response));
-    axios.get("http://localhost:4000/itineraries/")
+      axios.get("http://localhost:4000/itineraries/")
       .then(res => setItineraries(res.data.response))
+      
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
-  const getItineraries = () => {
+  const getItineraries = ()=>{
     let cityName = city.city
     let array = itineraries.filter(e => e.city === cityName)
-
     return array
   }
   const navigate = useNavigate();
@@ -29,23 +29,24 @@ export default function City() {
     <WebsiteLayout>
       <div className="city-main">
         <div className='city-container'>
-          <img src={city.photo} alt='city-img'></img>
+          <img className='city-img' src={city.photo} alt='city-img'></img>
           <div className='city-details'>
-            <h3 className='h3-city'>Description</h3>
-            <p className='description-p'>{city.description}</p>
             <ul>
-              <li className='tittle-city'>{city.city}</li>
-              <li className='p-city'>Country:{city.country}</li>
-              <li className='p-city'>Population:{city.population}</li>
-              <li className='p-city'>Foundation:{city.foundation}</li>
+              <li>Name:{city.city}</li>
+              <li>Country:{city.country}</li>
+              <li>Population:{city.population}</li>
+              <li>Foundation:{city.foundation}</li>
             </ul>
+            <h3>Description</h3>
+            <p className='description-p'>{city.description}</p>
           </div>
           <div>
-            {getItineraries().map(item => (<Itinerary data={item} />))}
+           {getItineraries().map(item=>(<Itinerary data={item}/>))}           
           </div>
-          <button className='bcity' onClick={() => navigate(-1)}>Go back</button>
+          <button className="goBack" onClick={() => navigate(-1)}>Go back</button>
         </div>
-      </div>
+      
+    </div >
     </WebsiteLayout >
   )
 }
