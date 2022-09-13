fa59b892db157cb4e917814139c19fc3fdb00ba1
danielgonzalez22 <kavellid63@gmail.com>
2022-09-13 20:31:48 -0300

    correcion de query en pagina mytineraries

--- src/pages/MyTineraries.js
+++ src/pages/MyTineraries.js
@@ -4,33 +4,15 @@ import { useGetItinerariesUsersQuery } from '../features/actions/itinerariesApi'
 import Itinerary from '../components/Itinerary/Itinerary'
 
 export default function MyTineraries() {
-    
-    let { data: items,
-        isLoading,
-        isSuccess,
-        isFailed, } = useGetItinerariesUsersQuery('6318f2cf86ea60a969d24e49')
-      if (isLoading) {
-        items = []
-        console.log("Loading");
-      } else if (isSuccess) {
-        console.log("Load succesfully");
-      } else if (isFailed) {
-        console.log("3");
-        items = [];
-      }
-      let itineraries=items.response
-      const itemView = (item) => (
-        <Itinerary items={item}/>      )
-
-    return (
-        <WebsiteLayout>
+  let { data: items } = useGetItinerariesUsersQuery('6318f2cf86ea60a969d24e49')
+  return (
+    <WebsiteLayout>
       <div className='container-itineraries'>
-        
-        {itineraries.map((item) =><Itinerary items={item}/>)}
-        </div>
-      </WebsiteLayout>
-    )
-  }
+        {items?.map((item) => <Itinerary itinerary={item} />)}
+      </div>
+    </WebsiteLayout>
+  )
+}
 
 
 
