b7b8089fe1dc6f83645c3fc4c448cf37d5c17c7b
danielgonzalez22 <kavellid63@gmail.com>
2022-09-13 20:28:14 -0300

    componentes itinerary, comment y activity cambiados a representativos

--- src/components/Itinerary/Itinerary.js
+++ src/components/Itinerary/Itinerary.js
@@ -1,34 +1,35 @@
-// import Activities from "./Activities"
-// import Comments from "./Comments"
-// import Comments from "./Comment"
-// import Activities from "./Activity"
+import Activity from "./Activity"
+import Comment from "./Comment"
+import { useGetItinerariesCommentQuery } from '../../features/actions/commentsApi'
+import { useGetItineraryActivitiesQuery } from '../../features/actions/activitiesApi'
 import '../../styles/Itinerary/Itinerary.css'
 export default function Itinerary(props) {
-
-  const item = props.items
-   return (
+  const itinerary = props.itinerary
+  let { data: comments } = useGetItinerariesCommentQuery(itinerary._id)
+  let { data: activities } = useGetItineraryActivitiesQuery(itinerary._id)
+  return (
     <>
-      <p className="name-it">{item.name}</p>
+      <p className="name-it">{itinerary.name}</p>
       <div className="container-it">
         <div className="user-it">
-          <img src="https://www.cariverplate.com.ar/imagenes/jugadores/2022-08/1498-24-perez-imagenprincipal.png" alt="" className="img-user" />
-          <p>`${item.user.name} ${item.user.lastName}`</p>
-          <p>enzo123@gmail.com</p>
-          <p>Argentina</p>
+          <img src={itinerary.user.photo} alt="img-user" className="img-user" />
+          <p>{itinerary.user.name} {itinerary.user.lastName}</p>
+          <p>{itinerary.user.country}</p>
         </div>
         <div className="main-it">
-          <div className="text-it">
-            <p>{item.likes} Likes</p>
-            <p className="text-duration">Duration: {item.duration}hs</p>
-          </div>
-          <p className="price-it">Price: ${item.price}</p>
-          <p className="hashtags">
-            {item.tags.map(tag => tag)}
+          <p>{itinerary.likes} Likes</p>
+          <p className="text-duration">Duration: {itinerary.duration}hs</p>
+          <p className="price-it">Price: ${itinerary.price}</p>
+          <p className="hashtags">{itinerary.tags.map(tag => tag)}
           </p>
         </div>
-        {/* {<Comments itinerary={itinerary._id} />} */}
       </div>
-    {/* {<Activities itinerary={itinerary._id} />} */}
+      <div className="activities-it">
+        {activities ? ((activities.length > 0) ? activities.map(itinerary => <Activity activity={itinerary} />) : <p>No activities here...</p>) : <p>No activities here...</p>}
+      </div>
+      <div className="comments-it">
+        {comments ? ((comments.length > 0) ? comments.map(itinerary => <Comment comment={itinerary} />) : <p>No comments here...</p>) : <p>No comments here...</p>}
+      </div>
     </>
   )
 }
