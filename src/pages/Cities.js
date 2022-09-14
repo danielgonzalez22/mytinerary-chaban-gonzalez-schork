8986f1224361f0def1fbffa06d3fa030eb18381b
danielgonzalez22 <kavellid63@gmail.com>
2022-09-14 00:28:59 -0300

    quitado useRef para el search input

--- src/pages/Cities.js
+++ src/pages/Cities.js
@@ -1,22 +1,20 @@
 import '../styles/Cities.css';
-import { useState, useRef } from "react"
+import { useState } from "react"
 import WebsiteLayout from '../layouts/WebsiteLayout';
 import CityCard from '../components/CityCard';
 import { citiesAPI } from '../features/actions/citiesApi'
 
 function Cities() {
-const [cityName, setCityName] = useState("")
-    let { data: items} = citiesAPI.useGetAllCitiesQuery(cityName)
-    let searchRef = useRef()
+  const [cityName, setCityName] = useState("")
+  let { data: items } = citiesAPI.useGetAllCitiesQuery(cityName)
 
   const itemView = (item) => (
     <CityCard image={item.photo} title={item.city} id={item._id}></CityCard>
   )
-  const search = () =>{setCityName(searchRef.current.value)}
   return (
     <WebsiteLayout>
       <div className='cities-main'>
-      <input type="text" placeholder="Search..." onChange={search} ref={searchRef} className="cities-search"></input>
+        <input type="text" placeholder="Search..." onChange={(e) => setCityName(e.target.value)} className="cities-search"></input>
         <div className="cards-cities-container">
           {items?.map(itemView)}
         </div>
