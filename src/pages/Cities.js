c01b4b44f1fc06a5c2348c622d9cfebf37e77bd2
danielgonzalez22 <kavellid63@gmail.com>
2022-09-14 00:08:25 -0300

    añadido filtro de ciudades en pagina cities, funcionando con redux query

--- src/pages/Cities.js
+++ src/pages/Cities.js
@@ -1,5 +1,5 @@
 import '../styles/Cities.css';
-import { useState, useEffect } from "react"
+import { useState, useRef } from "react"
 import WebsiteLayout from '../layouts/WebsiteLayout';
 import CityCard from '../components/CityCard';
 import { citiesAPI } from '../features/actions/citiesApi'
@@ -7,14 +7,16 @@ import { citiesAPI } from '../features/actions/citiesApi'
 function Cities() {
 const [cityName, setCityName] = useState("")
     let { data: items} = citiesAPI.useGetAllCitiesQuery(cityName)
-    
+    let searchRef = useRef()
 
   const itemView = (item) => (
     <CityCard image={item.photo} title={item.city} id={item._id}></CityCard>
   )
+  const search = () =>{setCityName(searchRef.current.value)}
   return (
     <WebsiteLayout>
       <div className='cities-main'>
+      <input type="text" placeholder="Search..." onChange={search} ref={searchRef} className="cities-search"></input>
         <div className="cards-cities-container">
           {items?.map(itemView)}
         </div>
