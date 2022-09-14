0a2ba5e9a31147a3e4f2354e61bbfaab3efb6eaa
danielgonzalez22 <kavellid63@gmail.com>
2022-09-13 23:47:06 -0300

    correccion en query de citiesApi, componentes itinerary generados en city (details), estilos en city (details)

--- src/pages/City.js
+++ src/pages/City.js
@@ -1,53 +1,40 @@
 import WebsiteLayout from '../layouts/WebsiteLayout'
 import '../styles/City.css'
-import React, { useEffect, useState } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
-import axios from 'axios'
 import Itinerary from "../components/Itinerary/Itinerary"
-
+import { useGetOneCityQuery } from '../features/actions/citiesApi'
+import { useGetCityItinerariesQuery } from '../features/actions/itinerariesApi'
 
 export default function City() {
-  const [city, setCity] = useState({})
-  const [itineraries, setItineraries] = useState([])
   const params = useParams()
   const { id } = params
-  useEffect(() => {
-    axios.get("http://localhost:4000/cities/" + [id])
-      .then(res => setCity(res.data.response));
-      axios.get("http://localhost:4000/itineraries/")
-      .then(res => setItineraries(res.data.response))
-      
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-  const getItineraries = ()=>{
-    let cityName = city.city
-    let array = itineraries.filter(e => e.city === cityName)
-    return array
-  }
+  let { data: city} = useGetOneCityQuery(id)
+  let { data: itineraries} = useGetCityItinerariesQuery(id)
+  
   const navigate = useNavigate();
 
   return (
     <WebsiteLayout>
-      <div className="city-main">
+      {city?
         <div className='city-container'>
           <img className='city-img' src={city.photo} alt='city-img'></img>
           <div className='city-details'>
             <ul>
-              <li>Name:{city.city}</li>
-              <li>Country:{city.country}</li>
-              <li>Population:{city.population}</li>
-              <li>Foundation:{city.foundation}</li>
+              <li>Name: {city.city}</li>
+              <li>Country: {city.country}</li>
+              <li>Population: {city.population}</li>
+              <li>Foundation: {city.foundation}</li>
             </ul>
             <h3>Description</h3>
             <p className='description-p'>{city.description}</p>
           </div>
           <div>
-           {getItineraries().map(item=>(<Itinerary data={item}/>))}           
+           {itineraries?((itineraries.length>0)? itineraries.map(itinerary=><Itinerary itinerary={itinerary}/>): <p>No itineraries here...</p>): <p>No itineraries here...</p>}           
           </div>
           <button className="goBack" onClick={() => navigate(-1)}>Go back</button>
         </div>
-      
-    </div >
+    :<p>City not found on database</p>
+    }
     </WebsiteLayout >
   )
 }
