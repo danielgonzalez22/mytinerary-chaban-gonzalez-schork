feadbef942a7c92020c421534ed238fb3f186e74
danielgonzalez22 <kavellid63@gmail.com>
2022-09-16 07:26:13 -0300

    implementacion de logica de redux en componente login (signIn)

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,21 +1,41 @@
 
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useRef } from 'react'
 import Modal from './Modal';
 import Input from './Input';
 import '../styles/SignUp.css'
 import { Link as LinkRouter } from 'react-router-dom'
-
-
+import { useUserSignInMutation } from '../features/actions/usersApi'
 
 const SignIncomponent = () => {
+  const [logUser, { data: body, error, isSuccess }] = useUserSignInMutation()
+  let alertMessage = ""
+  if (body?.success) {
+    alertMessage = body?.message
+    console.log(body.success)
+  } else if (error) {
+    alertMessage = error?.data.message
+    console.log(isSuccess)
+  }
   const [isOpen, setIsOpen] = useState(false);
-  const openModal = () => setIsOpen(true);
   const closeModal = () => setIsOpen(false);
+  const email = useRef()
+  const password = useRef()
+
+  const handleSubmit = (e) => {
+    email.preventDefault()
+    const user = {
+      mail: email.current.value,
+      password: password.current.value
+    }
+    logUser(user)
+    setIsOpen(true)
+    setTimeout(() => {
+      setIsOpen(false)
+    }, 5000)
+  }
   return (
-
-
     <>      <Modal isOpen={isOpen}
-      closeModal={closeModal} text="Account Created Succesfully!" />
+      closeModal={closeModal} text={alertMessage} result={isSuccess} />
       <div className="signUp-main">
         <div className='signUp-container'>
           <div className="signUp-body">
@@ -26,28 +46,21 @@ const SignIncomponent = () => {
           <h1 className="h1-form">
             Login<span className="my-style"> </span>
           </h1>
-
-
-          <form className="signUp-form" >
-            <Input text="Email"></Input>
-            <Input text="Password" ></Input>
+          <form className="signUp-form" onSubmit={handleSubmit}>
+            <Input text="Email" reference={email}></Input>
+            <Input text="Password" reference={password}></Input>
             <div>
-
               <div className='signUp-Container Buttons-containerV0FINAL'>
-                <button onClick={openModal} className="signUp-button" >Login</button>
-                <button onClick={openModal} className="signUp-button" >Google</button>
+                <button className="signUp-button" >Login</button>
               </div>
               <div className='button-container'>
-                <LinkRouter to='/' className="singUp1-button">Please, Sign Up!!</LinkRouter>
+                <LinkRouter to='/auth/signup' className="singUp1-button">Not have an account? Please sign up</LinkRouter>
               </div>
             </div>
           </form>
         </div>
-
       </div>
     </>
-
   )
 }
-
 export default SignIncomponent
