3788abfee60a5dab76133d3ea01cf53bf76ec444
David Chaban <davidchaban01@gmail.com>
2022-09-15 22:56:21 -0300

    Se agrega responsive a Paginas NewTineraries, SignUp y Login

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -19,27 +19,29 @@ const SignIncomponent = () => {
     <div className="SignUp-main">
         <div className='SignUp-container'>
       <div className="SignUp-body">
-        <div className="SignUp-img">
+      
           <img src="https://img.icons8.com/external-bearicons-outline-color-bearicons/344/external-sign-up-call-to-action-bearicons-outline-color-bearicons-2.png" alt="icon" className='Sign-img' />
-          <h1 className="h1-form">
+        </div>
+        <h1 className="h1-form">
             Login<span className="my-style"> </span>
           </h1>
-        </div>
-        </div>
-        </div>
 
 
             <form className="SignUp-form" >
               <Input text="Email"></Input>
               <Input text="Password" ></Input>
-             
-              <div className='SignUp-Container'>
+             <div>
+
+              <div className='SignUp-Container Buttons-containerV0FINAL'>
               <button onClick={openModal} className="SignUp-button" >Login</button>
               <button onClick={openModal} className="SignUp-button" >Google</button>
               </div>
-                <LinkRouter to='/' className="SingUp1-button">Sign in with Google</LinkRouter>
-                <LinkRouter to='/' className="SingUp1-button">Sign in with Email</LinkRouter>
+              <div className='button-container'>
+                <LinkRouter to='/' className="SingUp1-button">Please, Sign Up!!</LinkRouter>
+              </div>
+             </div>
             </form>
+        </div>
       
          </div>
          </>
--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -18,11 +18,11 @@ const closeModal = () => setIsOpen(false);
     
    <>      <Modal isOpen={isOpen} 
    closeModal={closeModal} text="Account Created Succesfully!"/>
-    <div className="SignUp-main">
+    <div className="SignUp-main Signup-mainV2">
         <div className='SignUp-container'>
       <div className="SignUp-body">
         <div className="SignUp-img">
-          <img src="https://img.icons8.com/external-bearicons-outline-color-bearicons/344/external-sign-up-call-to-action-bearicons-outline-color-bearicons-2.png" alt="icon" className='Sign-img' />
+          <img src="https://img.icons8.com/external-bearicons-outline-color-bearicons/344/external-sign-up-call-to-action-bearicons-outline-color-bearicons-2.png" alt="icon" className='Sign-img SignUp-imgV02' />
           <h1 className="h1-form">
             SIGN UP<span className="my-style"> </span>
           </h1>
@@ -52,13 +52,12 @@ const closeModal = () => setIsOpen(false);
               <Input text="URL Photo "></Input>
               <Input text="Email Address" ></Input>
               <Input text="Create Password" ></Input>
-              <div className='SignUp-Container'>
+              <div className='SignUp-Container Buttons-containerV0FINAL'>
               <button onClick={openModal} className="SignUp-button" >Create Account</button>
               <button onClick={openModal} className="SignUp-button" >Google</button>
               </div>
            
-                <LinkRouter to='/' className="SingUp1-button">You have an account?</LinkRouter>
-                <LinkRouter to='/' className="SingUp1-button">Please sign in</LinkRouter>
+                <LinkRouter to='/' className="SingUp1-button">You have an account?Please sign in</LinkRouter>
             </form>
       }
          </div>
