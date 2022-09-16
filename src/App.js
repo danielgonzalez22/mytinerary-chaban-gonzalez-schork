a01ef94edf4b88f48ed57e71874683643391d104
Mauro Schork <mauroschork107@hotmail.com>
2022-09-15 21:10:13 -0300

    se modifica archivo App

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import React from "react";
 import SignUp from "./pages/SignUp"
 import Login from "./pages/Login"
-import SignIn from "./pages/SignIn"
 import NewItinerary from "./pages/NewItinerary"
 
 function App() {
@@ -20,15 +19,13 @@ function App() {
     
         <Route path='/' element={<WelcomePage/>}></Route>
         <Route path='/auth/signup' element={<SignUp/>}></Route>
-        <Route path='/auth/signin' element={<SignIn/>}></Route>
+        <Route path='/auth/login' element={<Login/>}></Route>
         <Route path='/Cities' element={<Cities/>}></Route>
         <Route path='/NewCity' element={<NewCity/>}></Route>
         <Route path='/City/:id' element={<City/>}></Route>
         <Route path='/EditCity' element={<EditCity/>}></Route>
         <Route path='/MyTineraries' element={<MyTineraries/>}></Route>
         <Route path='/*' element={<NotFound/>}></Route>
-        <Route path='/SignUp' element={<SignUp/>}></Route>
-        <Route path='/Login' element={<Login/>}></Route>
         <Route path='/NewItinerary' element={<NewItinerary/>}></Route>
 
 
