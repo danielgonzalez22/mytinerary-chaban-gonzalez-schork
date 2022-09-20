fc18da4e2f504343352fa89ebdd4eefb3c1ae292
danielgonzalez22 <kavellid63@gmail.com>
2022-09-20 15:31:10 -0300

    correcciones en api.js (ruta de heroku reemplazada por localhost para trabajar), en package-lock.json y package.json (conflicto con dependencias 'serve' y 'store', aceptados ambos cambios)

--- src/api.js
+++ src/api.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-let apiurl = 'https://mytinerary-back-cgs.herokuapp.com'
-
-export default  apiurl
-=======
 const apiUrl = "http://localhost:4000"
 
 export default apiUrl
->>>>>>> 951d8f7da08bdd796a20f773a6d6f6377fbd2a6c
