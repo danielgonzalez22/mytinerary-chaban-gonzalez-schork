ef6bdca8b7eb9a1cc7315f3aa6e173b95efca503
danielgonzalez22 <kavellid63@gmail.com>
2022-09-22 00:23:07 -0300

    pequeño refactorizado en header, se agrega renderizado condicional en opciones de menu hamburguesa

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,14 @@
 import "../styles/Header.css"
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import BurgerMenu from "./BurgerMenu"
 import { Link as LinkRouter } from 'react-router-dom'
 import UserMenu from "./UserMenu"
+
 function Header() {
   const [loggedUserRole, setLoggedUserRole] = useState(localStorage.getItem("userRole"))
   useEffect(() => {
     setLoggedUserRole(localStorage.getItem("userRole"))
-  },[])
+  }, [])
   return (
     <div className="header-container">
       <div className="logo-nav">
@@ -16,22 +17,19 @@ function Header() {
         <nav>
           <LinkRouter to='/' className="navLink">Home</LinkRouter>
           <LinkRouter to='/Cities' className="navLink">Cities</LinkRouter>
-          {loggedUserRole ?
+          {loggedUserRole &&
             <LinkRouter to='/MyTineraries' className="navLink">MyTineraries</LinkRouter>
-            : null
           }
-          {loggedUserRole === "admin" ?
+          {loggedUserRole === "admin" &&
             <>
               <LinkRouter to='/NewCity' className="navLink">NewCity</LinkRouter>
               <LinkRouter to='/EditCity' className="navLink">EditCity</LinkRouter>
             </>
-            : null
           }
         </nav>
       </div>
       <div className="accounts">
         <UserMenu />
-
       </div>
     </div>
   )
