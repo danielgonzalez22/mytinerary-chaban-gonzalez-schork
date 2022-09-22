6e5d66533c5f3d2711375c0c2a8985045302528f
danielgonzalez22 <kavellid63@gmail.com>
2022-09-22 00:21:39 -0300

    se agrega cerrado de menu hamburguesa y menu de usuario al clickear en cualquier parte de la pagina

--- src/components/BurgerMenu.js
+++ src/components/BurgerMenu.js
@@ -1,29 +1,44 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import "../styles/Header.css"
 import { Link as LinkRouter } from 'react-router-dom'
 
-const pages = [
-  { name: 'Home', to: '/' },
-  { name: 'Cities', to: '/Cities' },
-  { name: 'NewCity', to: '/NewCity' },
-  { name: 'EditCity', to: '/EditCity' }
-]
-const navLinks = (page) => <LinkRouter className='navLink' to={page.to} key={page.name}>{page.name}</LinkRouter>
+const loggedUserRole = localStorage.getItem("userRole")
 
 export default function BurgerMenu() {
-  const [open, setOpen] = useState(false)
-  const handleOpenMenu = () => {
-    if (open === true) { setOpen(false) }
-    else { setOpen(true) }
+  let menuContent
+  if (loggedUserRole) {
+    menuContent = ([<LinkRouter className='navLink' to='/MyTineraries' key='MyTineraries'>MyTineraries</LinkRouter>])
   }
+  if (loggedUserRole === 'admin') {
+    menuContent = ([...menuContent,
+    <LinkRouter to='/NewCity' className="navLink" key='NewCity'>NewCity</LinkRouter>,
+    <LinkRouter to='/EditCity' className="navLink" key='EditCity'>EditCity</LinkRouter>
+    ])
+  }
+
+  const [open, setOpen] = useState(false)
+  const menuIcon = useRef()
+  const handleToggleMenu = () => { setOpen(!open) }
+  const handleCloseMenu = (e) => {
+    const isClickInside = menuIcon.current.contains(e.target)
+    if (menuIcon.current !== e.target && !isClickInside) {
+      setOpen(false)
+    }
+  }
+  useEffect(() => {
+    document.addEventListener('click', handleCloseMenu)
+    return () => document.removeEventListener('click', handleCloseMenu)
+  }, [])
   return (
     <div className='burger-nav'>
-      <button className='burger-button' onClick={handleOpenMenu}><img className='burger-img' src='/img/burger.svg' alt='burgerImg' />
+      <button ref={menuIcon} className='burger-button' onClick={handleToggleMenu}><img className='burger-img' src='/img/burger.svg' alt='burgerImg' />
         <div>
           {
-            open ? <div className='burger-menu'>
-              {pages.map(navLinks)}
-            </div> : null
+            open && <div className='burger-menu'>
+              <LinkRouter to='/' className="navLink" key='Home'>Home</LinkRouter>
+              <LinkRouter to='/Cities' className="navLink" key='Cities'>Cities</LinkRouter>
+              {menuContent}
+            </div>
           }
         </div>
       </button>
--- src/components/UserMenu.js
+++ src/components/UserMenu.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { Link as LinkRouter, useNavigate } from 'react-router-dom'
 
 const UserMenu = () => {
@@ -15,7 +15,7 @@ const UserMenu = () => {
   const navigate = useNavigate();
   const navLinks = (page) => <LinkRouter className='navLink' to={page.to} key={page.name}> {page.name} </LinkRouter>
   const userLogged = localStorage.getItem("userId")
-
+  const menuIcon = useRef()
   const handleLogOut = () => {
     localStorage.clear()
     navigate("/")
@@ -23,18 +23,24 @@ const UserMenu = () => {
   let menuContent
   if (userLogged) {
     menuContent = (<button onClick={handleLogOut}>Log Out</button>)
-  }
-  else {
+  } else {
     menuContent = (pages.map(navLinks))
   }
   const [open, setOpen] = useState(false)
-  const handleOpenMenu = () => {
-    if (open === true) { setOpen(false) }
-    else { setOpen(true) }
+  const handleToggleMenu = () => { setOpen(!open) }
+  const handleCloseMenu = (e) => {
+    const isClickInside = menuIcon.current.contains(e.target)
+    if (menuIcon.current !== e.target && !isClickInside) {
+      setOpen(false)
+    }
   }
+  useEffect(() => {
+    document.addEventListener('click', handleCloseMenu)
+    return () => document.removeEventListener('click', handleCloseMenu)
+  }, [])
   return (
     <div className='user-nav'>
-      <button className='user-button' onClick={handleOpenMenu}> <img src="/img/user.svg" alt="accountIcon" className='user-img' />
+      <button ref={menuIcon} className='user-button' onClick={handleToggleMenu}> <img src="/img/user.svg" alt="accountIcon" className='user-img' />
         <div>
           {
             open ? <div className='burger-menu'>
