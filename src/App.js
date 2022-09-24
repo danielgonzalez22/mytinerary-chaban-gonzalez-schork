661694bf6ad8c661a5e21cfd4146387dfd7de7fa
danielgonzalez22 <kavellid63@gmail.com>
2022-09-24 00:39:49 -0300

    añadida proteccion de rutas segun logueo y rol de usuario

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useSelector } from 'react-redux';
+import { useState, useEffect } from 'react'
 import MyTineraries from "./pages/MyTineraries"
 import WelcomePage from "./pages/WelcomePage"
 import Cities from "./pages/Cities"
@@ -5,26 +8,30 @@ import NewCity from "./pages/NewCity"
 import City from "./pages/City"
 import NotFound from "./pages/NotFound"
 import EditCity from "./pages/EditCity"
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import React from "react";
 import SignUp from "./pages/SignUp"
 import Login from "./pages/Login"
 import NewItinerary from "./pages/NewItinerary"
 
 function App() {
+  const isLogged = useSelector(state => state.isLogged)
+  const [loggedUserRole, setLoggedUserRole] = useState(localStorage.getItem("userRole"))
+  useEffect(() => {
+    setLoggedUserRole(localStorage.getItem("userRole"))
+  }, [isLogged])
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<WelcomePage />}></Route>
-        <Route path='/auth/signup' element={<SignUp />}></Route>
-        <Route path='/auth/login' element={<Login />}></Route>
         <Route path='/Cities' element={<Cities />}></Route>
-        <Route path='/NewCity' element={<NewCity />}></Route>
         <Route path='/City/:id' element={<City />}></Route>
-        <Route path='/EditCity' element={<EditCity />}></Route>
-        <Route path='/MyTineraries' element={<MyTineraries />}></Route>
+        <Route path='/MyTineraries' element={loggedUserRole ? <MyTineraries /> : <WelcomePage />}></Route>
+        <Route path='/NewItinerary/:id' element={loggedUserRole ? <NewItinerary /> : <WelcomePage />}></Route>
+        <Route path='/NewCity' element={loggedUserRole === 'admin' ? <NewCity /> : <WelcomePage />}></Route>
+        <Route path='/EditCity' element={loggedUserRole === 'admin' ? <EditCity /> : <WelcomePage />}></Route>
+        <Route path='/auth/signup' element={!loggedUserRole || loggedUserRole === 'admin' ? <SignUp /> : <WelcomePage />}></Route>
+        <Route path='/auth/login' element={!loggedUserRole ? <Login /> : <WelcomePage />}></Route>
         <Route path='/*' element={<NotFound />}></Route>
-        <Route path='/NewItinerary/:id' element={<NewItinerary />}></Route>
       </Routes>
     </BrowserRouter>
   )
